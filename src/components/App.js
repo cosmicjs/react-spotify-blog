6b107630d37293f6f6101b129917489108356efc
Michael Bruns <mjb77@georgetown.edu>
2018-09-01 10:00:10 -0500

    have basic grid set up for desktop. Trying to make responsive for mobile while still allowing width or 2 vs 1 on columns

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Old School Shuffle</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="featuredPost">
+          <p>Featured post here</p>
+        </div>
+        <div className="spotifyPlayer">
+          <p>spotifyPlayer here</p>
+        </div>
+        <div className="otherPosts">
+          <p>other posts here</p>
+        </div>
+        <div className="footer">
+          <p>cosmic footer here</p>
+        </div>
       </div>
     );
   }
