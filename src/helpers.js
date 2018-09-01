89265ff72d207272a63cdf77a463d1e86a5b9ad6
Michael Bruns <mjb77@georgetown.edu>
2018-09-01 17:31:29 -0500

    cleaned up Cosmic JS imported data format

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,26 @@
 import Cosmic from 'cosmicjs';
 
 export async function getCosmicJsData() {
-	console.log("process.env.REACT_APP_COSMIC_BUCKET ",process.env.REACT_APP_COSMIC_BUCKET);
-	console.log("process.env.NODE_ENV ",process.env.NODE_ENV);
-
-
     const api = Cosmic()
     const bucket = api.bucket({
       slug: process.env.REACT_APP_COSMIC_BUCKET
     })
-    const objects = (await bucket.getObjects()).objects;
-    console.log("objects ''",objects);
-    return objects
+    const arrayOfAllObjectsInBucket = (await bucket.getObjects()).objects;
+
+    return organizeCosmicJsDataByObjectType(arrayOfAllObjectsInBucket);
   }
+
+function organizeCosmicJsDataByObjectType(arrayOfCosmicJsData) {
+  //maps through array of objects, returns object with property for
+  //each slug-type, with the value being an array of the objects
+
+  let organizedData = {};
+  arrayOfCosmicJsData.map((object) => {
+    const typeSlug = object.type_slug;
+    organizedData.hasOwnProperty(typeSlug) ? '': organizedData[typeSlug] = [];
+    organizedData[typeSlug].push(object)
+  })
+  return organizedData;
+}
+
+
