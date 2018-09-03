7c9192033814430fcad8c1c9401a20815f9bd445
Michael Bruns <mjb77@georgetown.edu>
2018-09-03 10:03:55 -0500

    changed playlist chooser to menu and tied to state so ready to use on backend. Also learned that have to set environmental variables with command line and can't use .env file for some reason.

--- src/components/PlaylistChooser.js
+++ src/components/PlaylistChooser.js
@@ -4,6 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -37,33 +39,44 @@ class PlaylistChooser extends Component {
 
   constructor(props){
     super(props)
-    console.log("props from PlaylistChooser constructor",props);
     this.classes = props;
       //byAlbum or byArtist
       this.state = {
       byAlbumOrByArtist: 'byAlbum',
       addRelatedDiscography: "false",
-      playlists: []
+      playlists: [],
+      chosenPlaylistIndex: 0,
+      chosenPlaylistName: 'Choose a playlist',
+      anchorElement: null
     }
   }
 
   changeByAlbumOrByArtist(e){
     e.preventDefault()
-    console.log("e.target.value from changeByAlbumOrByArtist",e.target.value);
     this.setState({byAlbumOrByArtist: e.target.value})
   }
 
   changeAddRelatedDiscography(e){
     e.preventDefault()
-    console.log("e.target.value from changeAddRelatedDiscography",e.target.value);
     this.setState({addRelatedDiscography: e.target.value})
+  }
+
+  handlePlaylistChooserTopLevelClick(e) {
+    this.setState({anchorElement: e.currentTarget})
+  }
+
+  handlePlaylistMenuClick(e, playlistIndex){
+    this.setState({chosenPlaylistIndex: playlistIndex, chosenPlaylistName: this.state.playlists[playlistIndex].playlistName, anchorElement: null})
+  }
+
+  handlePlaylistMenuClose(e){
+    this.setState({anchorElement: null})
   }
 
   async componentDidMount() {
     await SpotifyFunctions.setAccessToken(this.props.accessToken);
     const playlists = await SpotifyFunctions.getUserPlaylists();
     this.setState({playlists: playlists});
-    // console.log("playlists from PlaylistChooser",playlists);    
   }
 
   generateListItem(playlistObj) {
@@ -74,12 +87,30 @@ class PlaylistChooser extends Component {
     )
   }
 
+  generateMenuItem(playlistObj, index) {
+    return (
+      <MenuItem key={playlistObj.id} selected={index === this.state.chosenPlaylistIndex} onClick={(e) => {this.handlePlaylistMenuClick(e, index)}}>
+            {playlistObj.playlistName}
+      </MenuItem>
+    )
+  }
+
 render() {
     return (
       <div className={`PlaylistChooser ${this.classes}`}>
         <List className={this.classes.playlistViewer}>
-          {this.state.playlists.map((playlistObj) => this.generateListItem(playlistObj))}
+          <ListItem button onClick={(e) => this.handlePlaylistChooserTopLevelClick(e)}>
+            <ListItemText primary="Choose a playlist to shuffle" secondary={this.state.chosenPlaylistName} />
+          </ListItem>
         </List>
+        <Menu 
+          id="lock-menu" 
+          anchorelement={this.state.anchorElement} 
+          open={Boolean(this.state.anchorElement)} 
+          onClose={(e) => this.handlePlaylistMenuClose(e)}
+        >
+          {this.state.playlists.map((playlistObj, index) => {return this.generateMenuItem(playlistObj, index)})}
+        </Menu>
         <div className={this.classes.optionsSelection}>
         <FormControl component="fieldset" className={this.classes.optionsSelection}>
           <FormLabel component="legend">Shuffle Options</FormLabel>
@@ -107,76 +138,48 @@ render() {
 
         </div>
       </div>
-
-
-    );
+    )
   }
-
-  // render() {
-  //   return (
-  //     <div className={`PlaylistChooser ${this.classes}`}>
-  //       <List className={this.classes.playlistViewer}>
-  //         <ListItem>
-  //           <ListItemText primary="First playlist" />
-  //         </ListItem>
-  //         <ListItem>
-  //           <ListItemText primary="Second playlist" />
-  //         </ListItem>
-  //         <ListItem>
-  //           <ListItemText primary="Third playlist" />
-  //         </ListItem>
-  //         <ListItem>
-  //           <ListItemText primary="Fourth playlist" />
-  //         </ListItem>
-  //         <ListItem>
-  //           <ListItemText primary="Fifth playlist" />
-  //         </ListItem>
-  //         <ListItem>
-  //           <ListItemText primary="Sixth playlist" />
-  //         </ListItem>
-  //         <ListItem>
-  //           <ListItemText primary="Seventh playlist" />
-  //         </ListItem>
-  //         <ListItem>
-  //           <ListItemText primary="Eighth playlist" />
-  //         </ListItem>
-  //         <ListItem>
-  //           <ListItemText primary="Ninth playlist" />
-  //         </ListItem>
-  //       </List>
-  //       <div className={this.classes.optionsSelection}>
-  //       <FormControl component="fieldset" className={this.classes.optionsSelection}>
-  //         <FormLabel component="legend">Shuffle Options</FormLabel>
-  //         <RadioGroup
-  //           aria-label="By Artist or By Album"
-  //           name="byAlbumOrByArtist"
-  //           value={this.state.byAlbumOrByArtist}
-  //           onChange={(e) => this.changeByAlbumOrByArtist(e)}
-  //         >
-  //           <FormControlLabel value="byAlbum" control={<Radio />} label="By Album" />
-  //           <FormControlLabel value="byArtist" control={<Radio />} label="By Artist" />
-  //         </RadioGroup>
-  //         <RadioGroup
-  //           aria-label="Add Related Discography"
-  //           name="addRelatedDiscography"
-  //           value={this.state.addRelatedDiscography}
-  //           onChange={(e) => this.changeAddRelatedDiscography(e)}
-  //         >
-  //           <FormControlLabel value="true" control={<Radio />} label="Include" />
-  //           <FormControlLabel value="false" control={<Radio />} label="Just my Playlist" />
-  //         </RadioGroup>
-
-
-  //       </FormControl>
-
-  //       </div>
-  //     </div>
-
-
-  //   );
-  // }
 }
 
+  
 export default withStyles(styles)(PlaylistChooser);
-// export default PlaylistChooser;
 
+
+// render() {
+//     return (
+//       <div className={`PlaylistChooser ${this.classes}`}>
+//         <List className={this.classes.playlistViewer}>
+//           {this.state.playlists.map((playlistObj) => this.generateListItem(playlistObj))}
+//         </List>
+//         <div className={this.classes.optionsSelection}>
+//         <FormControl component="fieldset" className={this.classes.optionsSelection}>
+//           <FormLabel component="legend">Shuffle Options</FormLabel>
+//           <RadioGroup
+//             aria-label="By Artist or By Album"
+//             name="byAlbumOrByArtist"
+//             value={this.state.byAlbumOrByArtist}
+//             onChange={(e) => this.changeByAlbumOrByArtist(e)}
+//           >
+//             <FormControlLabel value="byAlbum" control={<Radio />} label="By Album" />
+//             <FormControlLabel value="byArtist" control={<Radio />} label="By Artist" />
+//           </RadioGroup>
+//           <RadioGroup
+//             aria-label="Add Related Discography"
+//             name="addRelatedDiscography"
+//             value={this.state.addRelatedDiscography}
+//             onChange={(e) => this.changeAddRelatedDiscography(e)}
+//           >
+//             <FormControlLabel value="true" control={<Radio />} label="Include" />
+//             <FormControlLabel value="false" control={<Radio />} label="Just my Playlist" />
+//           </RadioGroup>
+
+
+//         </FormControl>
+
+//         </div>
+//       </div>
+
+
+//     );
+//   }
